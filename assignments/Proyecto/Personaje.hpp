41b4790c0cb073f042c74a04bd2df6aa6aad924d
Gp <157935064+JustGp@users.noreply.github.com>
2026-07-03 17:47:30 +0000

    hpp file

--- assignments/Proyecto/Personaje.hpp
+++ assignments/Proyecto/Personaje.hpp
@@ -1,1 +1,44 @@
 // Crear el archivo header de la clase Personaje, no olvides las guardas o el pragma.
+// A00844976 Gael Villatoro Perez
+
+#pragma once
+
+class Unidad
+{
+    public:
+    Unidad();
+    Unidad(int, int, int, int);
+
+    //getters
+    int getVida() const;
+    int getHpoints() const;
+    int getAtaque() const;
+    int getNivel() const;
+
+    //Setters
+    void setVida(int);
+    void setHpoints(int);
+    void setAtaque(int);
+    void setNivel(int);
+
+
+    //Methods
+    int porcentajeSalud();
+    void imprimirBarra();
+    int calculaAtaque(Unidad&);
+    void recibeAtaque(int);
+    void atacar(Unidad&);
+    void imprimir();
+
+    
+
+
+
+
+    private:
+    int vida, hpoints, ataque, nivel;
+
+
+
+
+};
