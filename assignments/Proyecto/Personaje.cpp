bd8cb03add9ef4763e705f57f820af8c94a613fe
Gp <157935064+JustGp@users.noreply.github.com>
2026-07-04 22:52:14 +0000

    Setters and getters

--- assignments/Proyecto/Personaje.cpp
+++ assignments/Proyecto/Personaje.cpp
@@ -1,1 +1,51 @@
 //GEnera la implementación de la clase base Personaje
+#pragma once
+#include "Personaje.hpp"
+#include <iostream>
+using namespace std;
+
+// Gael Emiliano VIllatoro Perez A00844976
+
+
+//Constructor 
+Unidad::Unidad(): vida{100}, hpoints{100}, ataque{10}, nivel{1}{}
+Unidad::Unidad(int valVida, int valHpoints, int valAtaque, int valNivel): vida{valVida}, hpoints{valHpoints}, ataque{valAtaque}, nivel{valNivel}{}
+
+// Getters
+
+int Unidad::getVida()const {
+    return vida;
+}
+
+int Unidad::getHpoints()const {
+    return hpoints;
+}
+
+int Unidad::getAtaque()const {
+    return ataque;
+}
+
+int Unidad::getNivel()const {
+    return nivel;
+}
+
+//Setters
+
+void Unidad::setVida(int valor) {
+    vida = valor;
+}
+
+void Unidad::setHpoints(int valor) {
+    hpoints = valor;
+}
+
+void Unidad::setAtaque(int valor) {
+    ataque = valor;
+}
+
+void Unidad::setNivel(int valor) {
+    nivel = valor;
+}
+
+
+//
